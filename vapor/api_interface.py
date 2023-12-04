ae4811dc4de2e220f68630ce2f24fc48dff02667
TabulateJarl8 <tabulatejarl8@gmail.com>
2023-12-04 13:10:26 -0500

    add ability to resolve steam vanity name into user ID

--- vapor/api_interface.py
+++ vapor/api_interface.py
@@ -10,11 +10,11 @@ async def get(session: aiohttp.ClientSession, url: str) -> Response:
 	"""Async get request for fetching web content.
 
 	Args:
-			session (aiohttp.ClientSession): an aiohttp session.
-			url (str): The URL to fetch data from.
+		session (aiohttp.ClientSession): an aiohttp session.
+		url (str): The URL to fetch data from.
 
 	Returns:
-			Response: A Response object containing the body and status code.
+		Response: A Response object containing the body and status code.
 	"""
 	async with session.get(url) as response:
 		return Response(data=await response.text(), status=response.status)
@@ -24,10 +24,10 @@ async def get_game_average_rating(id: str) -> str:
 	"""Get the average game rating from ProtonDB.
 
 	Args:
-			id (str): The game ID.
+		id (str): The game ID.
 
 	Returns:
-			str: A text rating from ProtonDB. gold, bronze, silver, etc.
+		str: A text rating from ProtonDB. gold, bronze, silver, etc.
 	"""
 	async with aiohttp.ClientSession() as session:
 		data = await get(
@@ -40,28 +40,67 @@ async def get_game_average_rating(id: str) -> str:
 		return json_data['tier']
 
 
-async def get_steam_user_data(api_key: str, id: str) -> SteamUserData:
-	"""Fetch a steam user's games and get their ratings from ProtonDB.
+async def resolve_vanity_name(api_key: str, name: str) -> str:
+	"""Resolve a Steam vanity name into a Steam user ID.
 
 	Args:
-			api_key (str): Steam API key.
-			id (str): The user's Steam ID.
+		api_key (str): The Steam API key.
+		name (str): The user's vanity name.
 
 	Raises:
-			InvalidIDError: If an invalid Steam ID is provided.
-			UnauthorizedError: If an invalid Steam API key is provided.
+		UnauthorizedError: If an invalid Steam API key is provided.
+		InvalidIDError: If an invalid Steam vanity URL is provided.
 
 	Returns:
-			SteamUserData: The Steam user's data.
+		str: The Steam ID of the user.
 	"""
 	async with aiohttp.ClientSession() as session:
 		data = await get(
 			session,
-			f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={id}&format=json&include_appinfo=1',
+			f'https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={api_key}&vanityurl={name}',
 		)
-		if 'Bad Request' in data.data:
+
+		if data.status == 403:
+			raise UnauthorizedError
+
+		user_data = json.loads(data.data)
+		if user_data['response']['success'] != 1:
 			raise InvalidIDError
-		if 'Unauthorized' in data.data:
+
+		return user_data['response']['steamid']
+
+
+async def get_steam_user_data(api_key: str, id: str) -> SteamUserData:
+	"""Fetch a steam user's games and get their ratings from ProtonDB.
+
+	Args:
+		api_key (str): Steam API key.
+		id (str): The user's Steam ID or vanity name.
+
+	Raises:
+		InvalidIDError: If an invalid Steam ID is provided.
+		UnauthorizedError: If an invalid Steam API key is provided.
+
+	Returns:
+		SteamUserData: The Steam user's data.
+	"""
+	# check if ID is a Steam ID or vanity URL
+	if len(id) != 17 or not id.startswith('76561198'):
+		try:
+			id = await resolve_vanity_name(api_key, id)
+		except UnauthorizedError as e:
+			raise UnauthorizedError from e
+		except InvalidIDError:
+			pass
+
+	async with aiohttp.ClientSession() as session:
+		data = await get(
+			session,
+			f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={id}&format=json&include_appinfo=1&include_played_free_games=1',
+		)
+		if data.status == 400:
+			raise InvalidIDError
+		if data.status == 401:
 			raise UnauthorizedError
 
 		games = json.loads(data.data)['response']['games']
